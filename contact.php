4715591791b29ef54914016f0e1676f507a46e4a
markdmy <markdmy@hotmail.com>
2023-09-28 14:12:55 -0400

    div fix for css

--- contact.php
+++ contact.php
@@ -15,20 +15,18 @@
 <body>
     <?php include('components/header.php'); ?>
     <main>
-<form class="contactForm" action="models/contact_model.php" method="POST" id="contactForm" class="">
+        <div class="centerContactForm">
+            <form class="contactForm" action="models/contact_model.php" method="POST" id="contactForm" class="">
                 <h2 class="h2ContactUs">Contact Us</h2>
                 <label for="name" class="form-label">Name:</label>
-                <input type="text" id="contact-name" placeholder="Your Name" name="contact-name" class="form-input"
-                    required>
+                <input type="text" id="contact-name" placeholder="Your Name" name="contact-name" class="form-input" required>
 
                 <label for="email" class="form-label">Email:</label>
-                <input type="email" id="contact-email" placeholder="Your Email" name="contact-email" class="form-input"
-                    required>
+                <input type="email" id="contact-email" placeholder="Your Email" name="contact-email" class="form-input" required>
 
                 <label for="phone" class="form-label">phone number(Optional)</label>
 
-                <input type="tel" id="contact-phone" name="contact-phone" placeholder="Format: 123-456-7890"
-                    pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" class="form-input">
+                <input type="tel" id="contact-phone" name="contact-phone" placeholder="Format: 123-456-7890" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" class="form-input">
 
                 <label for="message" class="form-label">Message:</label>
                 <textarea name="contact-comments" rows="4" required class="form-textarea"></textarea>
@@ -43,7 +41,7 @@
     </main>
 
     <?php
-include('components/footer.php'); ?>
+    include('components/footer.php'); ?>
 
     <script src="js/app.js"></script>
 </body>
