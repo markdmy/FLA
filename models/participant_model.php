a7a84c0aa3f59ab8128129040be491526bc2113e
Eunji Park <eunjipark1984@gmail.com>
2023-10-30 11:25:30 -0400

    more fixings ...

--- models/participant_model.php
+++ models/participant_model.php
@@ -213,7 +213,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             }
             
             //executing a function email contact@freelaundryaccess.com about registration form being submitted.
-            $redirectUrl = send_email_from_reg_form($firstName, $lastName, $dateOfBirth, $numberOfHousehold, $numberOfAdults, $NumberOfChildrenUnder12, $NumberOfChildrenOver12, $email, $address, $phone, $city, $province, $postalCode, $housing_situation, $combinedFoundProgram, $formCreated, $id_file_path, $income_proof_file_path, $familyMemberInfo);
+            $redirectUrl = send_email_from_reg_form($firstName, $lastName, $dateOfBirth, $numberOfHousehold, $numberOfAdults, $NumberOfChildrenUnder12, $NumberOfChildrenOver12, $email, $address, $phone, $city, $province, $postalCode, $housing_situation, $combinedFoundProgram,  $additionalNote, $formCreated, $id_file_path, $income_proof_file_path, $familyMemberInfo);
 
             if ($redirectUrl) {
                 echo "<script>window.location.href='$redirectUrl';</script>";
